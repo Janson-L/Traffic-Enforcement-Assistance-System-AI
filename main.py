1b20ddc274fa61906cc3eb4451f6d1a768b7435a
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-12 22:01:38 +0800

    Image conversion from b64 is done
    -Need to work on deleting it after finish processing
    -put model in it

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 from flask import Flask, request, jsonify
 import base64
+from PIL import Image
 app = Flask(__name__)
 
 def base64ToImg(base64Img):
-    img=base64.b64decode(base64Img)
-    print("Finised base64 Conversion")
-    return img
+    # img=base64.b64decode(base64Img)
+    # print("Finised base64 Conversion")
+    # return img
+    with open("tempImage.png", "wb") as fh:
+        fh.write(base64.b64decode(base64Img))
 
 @app.route("/", methods=["POST"])
 def CarPlateRecognition():
-    img = base64ToImg(request.json['img'])
-    return "hello \n"
+    base64ToImg(request.json['img'])
+    return "Hello! \n"
 
 if __name__ == "__main__":
     app.run(debug = True)
