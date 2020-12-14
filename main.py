60065e9241f1cc7e137217d9cdaba8b571ae7790
Janson-L <52344189+Janson-L@users.noreply.github.com>
2020-12-15 00:21:53 +0800

    Disable debug for prod host

--- main.py
+++ main.py
@@ -182,5 +182,5 @@ def CarPlateRecognition():
     
 
 if __name__ == "__main__":
-    #app.run(debug = True)
-    app.run(debug = True,host="0.0.0.0",port=5000)
+    app.run(debug = True)
+    #app.run(host="0.0.0.0",port=5000)
